8352432c84e13caf08790149a68c66d4d2d63948
EmmanuelSW <edhgt89@hotmail.com>
2019-10-18 01:15:07 -0500

    Meal item info and styles

--- meals/lib/utils/style.dart
+++ meals/lib/utils/style.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppStyle {
-
   static final TextStyle title = TextStyle(
     fontWeight: FontWeight.bold,
     fontSize: 18,
     color: Colors.black,
   );
 
+  static final TextStyle bold = TextStyle(
+    fontWeight: FontWeight.bold,
+  );
 }
--- meals/lib/widgets/meal_item.dart
+++ meals/lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/utils/style.dart';
 
 class MealItem extends StatelessWidget {
   final String title, imageUrl;
@@ -15,6 +16,38 @@ class MealItem extends StatelessWidget {
     @required this.complexity,
     @required this.affordability,
   }) : super(key: key);
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+      break;
+      case Complexity.Challenging:
+        return 'Challenging';
+      break;
+      case Complexity.Hard:
+        return 'Hard';
+      break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      break;
+      case Affordability.Pricey:
+        return 'Pricey';
+      break;
+      case Affordability.Luxurious:
+        return 'Luxurious';
+      break;
+      default:
+        return 'Unknown';
+    }
+  }
 
   void selectMeal() {}
 
@@ -43,9 +76,57 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 0,
+                  child: Container(
+                    width: 280,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(width: 3,),
+                      Text('$duration min', style: AppStyle.bold),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.work),
+                      SizedBox(width: 3,),
+                      Text(complexityText, style: AppStyle.bold),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 3,),
+                      Text(complexityText, style: AppStyle.bold),
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
