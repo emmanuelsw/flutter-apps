2f3aace1fab992e1d6ec9a11cbcd216dd35cc361
EmmanuelSW <edhgt89@hotmail.com>
2019-10-24 16:29:32 -0500

    Show dialog

--- sakurashop/lib/widgets/cart_item.dart
+++ sakurashop/lib/widgets/cart_item.dart
@@ -20,9 +20,33 @@ class CartItem extends StatelessWidget {
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from the cart?'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+              ),
+              FlatButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       background: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(topRight: Radius.circular(4), bottomRight: Radius.circular(4)),
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(4), bottomRight: Radius.circular(4)),
           color: Colors.red[300],
         ),
         child: Icon(
